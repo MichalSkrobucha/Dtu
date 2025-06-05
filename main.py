ffab00a9f8fd8f8bee37f9c3ab105f505b645379
Konrad <konrad.nowakowski@student.wat.edu.pl>
2025-06-05 12:40:26 +0200

    TODO pomysły

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def main() -> None:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+
+    # TODO: mieć AES taki który można łatwo modyfikować -> każda faza AES w poszczególnych funkcjach
+    # TODO: każdy powinien mieć swój pliczek python do symulacji uszkodzenia w działaniu AES oraz oraz jakieś funkcji do sprawdzania błedu / ew poprawienia
+    # TODO: zapytać 2 innych grup jak wygląda oddanie
+    # TODO: zrobić z tego ładną paczkę, requirements build itd.
+    # TODO: jakieś ładne GUI - Michał robi fajne (jak będzie czas)
