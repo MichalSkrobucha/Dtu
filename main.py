70187fe4568fe6be3d56ebf14a9374283200692a
micha <michal.skrobucha@gmail.com>
2025-06-06 16:52:33 +0200

    żeby ToDO widziało

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def main() -> None:
 if __name__ == '__main__':
     main()
 
-# TODO: mieć AES taki który można łatwo modyfikować -> każda faza AES w poszczególnych funkcjach
-# TODO: każdy powinien mieć swój pliczek python do symulacji uszkodzenia w działaniu AES oraz oraz jakieś funkcji do sprawdzania błedu / ew poprawienia
-# TODO: zrobić z tego ładną paczkę, requirements build itd.
-# TODO: zapytać 2 innych grup jak wygląda oddanie
-# TODO: jakieś ładne GUI - Michał robi fajne (jak będzie czas)
+# ToDo ma być z wcięciem bo inaczej nie widzi
+    # TODO: mieć AES taki który można łatwo modyfikować -> każda faza AES w poszczególnych funkcjach
+    # TODO: każdy powinien mieć swój pliczek python do symulacji uszkodzenia w działaniu AES oraz oraz jakieś funkcji do sprawdzania błedu / ew poprawienia
+    # TODO: zrobić z tego ładną paczkę, requirements build itd.
+    # TODO: zapytać 2 innych grup jak wygląda oddanie
+    # TODO: jakieś ładne GUI - Michał robi fajne (jak będzie czas)
